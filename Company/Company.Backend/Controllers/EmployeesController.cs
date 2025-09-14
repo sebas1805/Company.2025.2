87cd42a05fd5809744fbe5ca980ddba59439499a
Sebas <yhoan1853@gmail.com>
2025-09-14 14:22:02 -0500

    Add PUT and GET for employees

---
+++ Company/Company.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using Company.Backend.Data;
+using Company.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public EmployeesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Employees.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();
+        return Ok(employee);
+    }
+}
