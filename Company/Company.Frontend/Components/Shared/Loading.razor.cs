b5929079f8da872ed0e44d11506532d8353e12ec
Sebas <yhoan1853@gmail.com>
2025-10-12 19:08:14 -0500

    Create Shared components for filter and confirmdialog

---
+++ Company/Company.Frontend/Components/Shared/Loading.razor.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Company.Frontend.Components.Shared;
+
+public partial class Loading
+{
+    [Parameter] public string? Label { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        if (string.IsNullOrEmpty(Label))
+        {
+            Label = "Por favor espere...";
+        }
+    }
+}
